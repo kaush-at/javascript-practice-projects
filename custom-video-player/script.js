6b5a404f3871b58029e8e869779164725ecfbfd2
kaush <emailtokaush@gmail.com>
2020-05-08 00:42:18 -0400

    toggleVideoStatus() function and updatePlayIcon() function added

--- custom-video-player/script.js
+++ custom-video-player/script.js
@@ -4,6 +4,24 @@ const stop = document.getElementById("stop");
 const progress = document.getElementById("progress");
 const timestamp = document.getElementById("timestamp");
 
+//play and pause video
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+//update play/pause icon
+function updatePlayIcon() {
+  if (video.paused) {
+    play.innerHTML = `<i class="fa fa-play fa-2x"> </i>`;
+  } else {
+    play.innerHTML = `<i class="fa fa-pause fa-2x"> </i>`;
+  }
+}
+
 //Update progress and timestamp
 function updateProgress() {
   progress.value = (video.currentTime / video.duration) * 100;
