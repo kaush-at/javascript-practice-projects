478e6eb22db0bca8646a78f50c5731c1938a0a4b
kaush <emailtokaush@gmail.com>
2020-05-10 20:10:51 -0400

    remove unwanted comments

--- exchange-rate-calculator/script.js
+++ exchange-rate-calculator/script.js
@@ -7,16 +7,16 @@ const rateEle = document.getElementById("rate");
 
 // fetch the exchange rate and update the dom
 function calculate() {
-    const currencyOne = currencyEle_1.value;
-    const currencyTwo = currencyEle_2.value;
+  const currencyOne = currencyEle_1.value;
+  const currencyTwo = currencyEle_2.value;
 
-    fetch(`https://api.exchangerate-api.com/v4/latest/${currencyOne}`)
-        .then((res) => res.json())
-        .then((data) => {
-            const rates = data.rates[currencyTwo];
-            rateEle.innerText = `1 ${currencyOne} = ${rates} ${currencyTwo}`;
-            amountTwo.value = (amountOne.value * rates).toFixed(2);
-        });
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currencyOne}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const rates = data.rates[currencyTwo];
+      rateEle.innerText = `1 ${currencyOne} = ${rates} ${currencyTwo}`;
+      amountTwo.value = (amountOne.value * rates).toFixed(2);
+    });
 }
 
 // event listners
@@ -25,17 +25,9 @@ currencyEle_2.addEventListener("change", calculate);
 amountOne.addEventListener("input", calculate);
 amountTwo.addEventListener("input", calculate);
 buttonSwap.addEventListener("click", () => {
-    const temp = currencyEle_1.value;
-    currencyEle_1.value = currencyEle_2.value;
-    currencyEle_2.value = temp;
-    calculate();
+  const temp = currencyEle_1.value;
+  currencyEle_1.value = currencyEle_2.value;
+  currencyEle_2.value = temp;
+  calculate();
 });
 calculate();
-//fetch example
-// function calculate() {
-//   fetch("items.json")  -> fetch karanne item.json eken
-//     .then((res) => res.json())  specify we need as json
-//     .then((data) => console.log(data)); // instead of print on console we can get those data and put in to body or what ever
-// }
-
-// calculate();
